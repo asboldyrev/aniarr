8b66dee0fa15e1c0a23c4d845bc43a9379dc5640
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-10 22:49:04 +0700

    Добавляет маршрут ручной загрузки релиза

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ReleaseDownloadController;
 use App\Http\Controllers\Api\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 // Сериалы
 Route::post('/series', [SeriesController::class, 'store']);
+
+// Релизы
+Route::post('/releases/{release}/download', [ReleaseDownloadController::class, 'store']);
