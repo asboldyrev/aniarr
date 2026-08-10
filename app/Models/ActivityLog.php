bd959e0ffebe73023040a369bf4f7ca9c4e55ca4
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-10 20:01:07 +0700

    Reshape ActivityLog model

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -10,48 +10,61 @@ use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
- * @property int $series_id
- * @property string $message
+ * @property int|null $series_id
+ * @property int|null $season_id
+ * @property int|null $download_id
+ * @property string|null $source
+ * @property string|null $event
  * @property LogType $type
+ * @property string $message
  * @property array|null $context
+ * @property Carbon|null $resolved_at
  * @property Carbon $created_at
  * @property Carbon $updated_at
- * @property-read Series $series
+ * @property-read Series|null $series
+ * @property-read Season|null $season
+ * @property-read Download|null $download
  */
 class ActivityLog extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'series_id',
-        'message',
+        'season_id',
+        'download_id',
+        'source',
+        'event',
         'type',
+        'message',
         'context',
+        'resolved_at',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
+            'series_id' => 'integer',
+            'season_id' => 'integer',
+            'download_id' => 'integer',
             'type' => LogType::class,
             'context' => 'array',
+            'resolved_at' => 'datetime',
         ];
     }
 
-    /**
-     * Get the series that owns the activity log.
-     */
     public function series(): BelongsTo
     {
         return $this->belongsTo(Series::class);
+    }
+
+    public function season(): BelongsTo
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+    public function download(): BelongsTo
+    {
+        return $this->belongsTo(Download::class);
     }
 }
