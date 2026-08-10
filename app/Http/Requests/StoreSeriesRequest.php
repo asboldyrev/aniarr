31c66ab2301a989f3e581aa5d3dad2c595f10d2c
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-11 00:23:08 +0700

    Упрощает контракт добавления Series

--- app/Http/Requests/StoreSeriesRequest.php
+++ app/Http/Requests/StoreSeriesRequest.php
@@ -5,32 +5,23 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreSeriesRequest extends FormRequest
+final class StoreSeriesRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return true; // Нет авторизации
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'title' => ['required', 'string', 'max:255'],
             'thetvdb_id' => ['required', 'integer', 'min:1'],
-            'thetvdb_slug' => ['nullable', 'string', 'max:255'],
-            'poster_url' => ['nullable', 'url', 'max:500'],
-            'year' => ['nullable', 'integer', 'min:1900', 'max:'.date('Y')],
             'rss_feeds' => ['required', 'array', 'min:1'],
             'rss_feeds.*.rss_url' => ['required', 'url', 'max:500'],
-            'rss_feeds.*.season_number' => ['nullable', 'integer', 'min:1'],
+            'rss_feeds.*.season_number' => ['nullable', 'integer', 'min:0'],
         ];
     }
 }
