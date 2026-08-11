d2a2c394c77fa337dd709e8522678f0c4f3773b5
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-11 15:14:38 +0700

    Связывает мобильную навигацию с оболочкой приложения

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,35 +1,29 @@
 <template>
     <div class="flex min-h-screen w-full flex-col bg-background md:flex-row" :class="{ dark: isDark }">
-        <MobileNav @Click="setAddDialogOpen(true)" />
-        <Sidebar @onAddClick="() => setAddDialogOpen(true)" />
-        <!-- <AppSidebar onAddClick={() => setAddDialogOpen(true)} /> -->
-        <main className="flex-1 overflow-auto">
+        <MobileNav @add-click="setAddDialogOpen(true)" />
+        <Sidebar @on-add-click="setAddDialogOpen(true)" />
+
+        <main class="min-w-0 flex-1 overflow-auto">
             <RouterView />
         </main>
-        <AddSeriesDialog :open="addDialogOpen" @onOpenChange="() => setAddDialogOpen(true)" />
-    </div>
 
+        <AddSeriesDialog :open="addDialogOpen" @on-open-change="setAddDialogOpen" />
+    </div>
 </template>
 
 <script setup>
-    import Sidebar from '@/components/Sidebar.vue';
-    import useThemeStore from '@/stores/ThemeStore';
-    import AddSeriesDialog from '@/components/AddSeriesDialog.vue';
-    import MobileNav from '@/components/MobileNav.vue';
-
-    import { computed, ref } from 'vue';
+    import { computed, ref } from 'vue'
+    import AddSeriesDialog from '@/components/AddSeriesDialog.vue'
+    import MobileNav from '@/components/MobileNav.vue'
+    import Sidebar from '@/components/Sidebar.vue'
+    import useThemeStore from '@/stores/ThemeStore'
 
     const themeStore = useThemeStore()
-
     const addDialogOpen = ref(false)
 
     function setAddDialogOpen(state) {
         addDialogOpen.value = state
-        console.log('set open add modal')
     }
 
-    const isDark = computed(() => {
-        return themeStore.isDark()
-    })
-
+    const isDark = computed(() => themeStore.isDark())
 </script>
