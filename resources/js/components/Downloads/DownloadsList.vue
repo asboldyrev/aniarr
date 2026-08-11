2c97d9c7ee343466a0cf5190aa7897ddc04e19bb
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-11 16:12:27 +0700

    Добавляет список Downloads

---
+++ resources/js/components/Downloads/DownloadsList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="space-y-3">
+        <DownloadItemCard
+            v-for="download in downloads"
+            :key="download.id"
+            :download="download"
+        />
+    </div>
+</template>
+
+<script setup>
+    import DownloadItemCard from '@/components/Downloads/DownloadItemCard.vue'
+
+    defineProps({
+        downloads: { type: Array, default: () => [] },
+    })
+</script>
