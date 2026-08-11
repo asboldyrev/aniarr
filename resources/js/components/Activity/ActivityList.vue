eb2029f90634c1ca8ed6ff2cb3a384a269cceb55
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-11 15:59:59 +0700

    Добавляет список Activity

---
+++ resources/js/components/Activity/ActivityList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="space-y-3">
+        <ActivityItem
+            v-for="item in items"
+            :key="item.id"
+            :item="item"
+            :busy="busyIds.has(item.id)"
+            @resolve="$emit('resolve', $event)"
+            @reopen="$emit('reopen', $event)"
+        />
+    </div>
+</template>
+
+<script setup>
+    import ActivityItem from '@/components/Activity/ActivityItem.vue'
+
+    defineProps({
+        items: { type: Array, default: () => [] },
+        busyIds: { type: Set, default: () => new Set() },
+    })
+
+    defineEmits(['resolve', 'reopen'])
+</script>
