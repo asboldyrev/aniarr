48c54e498b9c42234c440619c4acea2d64deb9e5
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-13 02:49:55 +0700

    Добавляет daily канал логов Aniarr

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'aniarr' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/aniarr.log'),
+            'level' => env('ANIARR_LOG_LEVEL', 'debug'),
+            'days' => env('ANIARR_LOG_DAILY_DAYS', env('LOG_DAILY_DAYS', 14)),
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
