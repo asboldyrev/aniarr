f7091f0224be417abb0eda001f4c3341d8155920
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-13 01:00:31 +0700

    Регистрирует realtime observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\ActivityLog;
+use App\Models\Download;
+use App\Models\RssFeed;
+use App\Models\Series;
+use App\Observers\ActivityLogObserver;
+use App\Observers\DownloadObserver;
+use App\Observers\RssFeedObserver;
+use App\Observers\SeriesObserver;
 use App\Services\Logging\AniarrLogger;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Series::observe(SeriesObserver::class);
+        RssFeed::observe(RssFeedObserver::class);
+        Download::observe(DownloadObserver::class);
+        ActivityLog::observe(ActivityLogObserver::class);
     }
 }
