fe9751328798037856ac42b98afefad8446867ef
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-13 00:59:43 +0700

    Добавляет общее realtime событие Aniarr

---
+++ app/Events/RealtimeChanged.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+final class RealtimeChanged implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public readonly string $resource,
+        public readonly string $action,
+        public readonly ?int $id = null,
+        public readonly ?int $seriesId = null,
+        public readonly ?int $seasonId = null,
+        public readonly ?int $downloadId = null,
+    ) {}
+
+    public function broadcastOn(): array
+    {
+        return [new Channel('aniarr')];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'realtime.changed';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'resource' => $this->resource,
+            'action' => $this->action,
+            'id' => $this->id,
+            'seriesId' => $this->seriesId,
+            'seasonId' => $this->seasonId,
+            'downloadId' => $this->downloadId,
+        ];
+    }
+}
