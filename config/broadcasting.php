dd36b2c779136adc18cfb0e545657c50e392a931
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-13 00:59:04 +0700

    Исправляет конфигурацию broadcasting Reverb

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,4 +13,17 @@ return [
                 'host' => env('REVERB_HOST', '127.0.0.1'),
                 'port' => env('REVERB_PORT', 8080),
                 'scheme' => env('REVERB_SCHEME', 'http'),
-                'useTLS' => env('RE
+                'useTLS' => env('REVERB_SCHEME', 'http') === 'https',
+            ],
+            'client_options' => [],
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+    ],
+];
